782813c91ed12b2637c056f081032f72e9a77c4d
Alice Wood <alicesophiewood@gmail.com>
2023-04-19 14:56:23 +0100

    test amended for 2 entries

--- spec/diary_integration_spec.rb
+++ spec/diary_integration_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe "Diary Integration" do
     it "returns the entries" do
       diary = Diary.new
       entry1 = DiaryEntry.new("title1", "contents1")
+      entry2 = DiaryEntry.new("title2", "contents2")
       diary.add(entry1)
-      expect(diary.all).to eq [entry1]
+      diary.add(entry2)
+      expect(diary.all).to eq [entry1, entry2]
     end
   end
 end
